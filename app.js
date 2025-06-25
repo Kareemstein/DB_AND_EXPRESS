c40c46af4199fbeda35bba1772409bdce8948a84
Kareem <kareemhuss@gmail.com>
2025-06-25 20:13:59 +0300

    edits 1.5

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const createDBConnection = require('./db');
+console.log("Environment:", process.env.NODE_ENV); 
+console.log("Pool is:", pool); 
 
 const app = express();
 app.use(express.json());
